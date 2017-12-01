fdd85ffb5cd027a0152937286dc76ddba2ad0170
Jun Li <june11084@gmail.com>
2017-12-01 12:15:15 -0800

    have functional 2 radio button selection section, added functional branchings

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,66 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
 $(document).ready(function() {
   $("form#quiz").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
-    var result;
-    if (operator === "add") {
-      result = add(number1, number2);
-    } else if (operator === "subtract") {
-      result = subtract(number1, number2);
-    } else if (operator === "multiply") {
-      result = multiply(number1, number2);
-    } else if (operator === "divide") {
-      result = divide(number1, number2);
+    var name = $("#name").val();
+    var ruby = 0;
+    var php = 0;
+    var java = 0;
+    var css = 0;
+    var c = 0;
+
+    var application = $("input:radio[name=application]:checked").val();
+    if (application === "ruby") {
+      ruby += 1;
+    } else if (application === "php") {
+      php += 1;
+    } else if (application === "java") {
+      java += 1;
+    } else if (application === "css") {
+      css += 1;
+    } else if (application === "c") {
+      c += 1;
     }
-    $("#output").text(result);
+
+    var company = $("input:radio[name=company]:checked").val();
+    if (company === "ruby") {
+      ruby += 1;
+    } else if (company === "php") {
+      php += 1;
+    } else if (company === "java") {
+      java += 1;
+    } else if (company === "css") {
+      css += 1;
+    } else if (company === "c") {
+      c += 1;
+    }
+
+    if(ruby != php != java != css != c){    //if all options are "not" selected the equal amount of times
+      var result = Math.max(ruby,php,java,css,c); //this is the only  way I could think of without using array
+      if(result === ruby){
+        ruby = "Ruby/Rails";
+        $("#result").text(name + ", you are most compatible with: " + ruby);
+      }
+      if(result === php){
+        php = "PHP/Drupal";
+        $("#result").text(name + ", you are most compatible with: " + php);
+      }
+      if(result === java){
+        java = "Java/Android";
+        $("#result").text(name + ", you are most compatible with: " + java);
+      }
+      if(result === css){
+        css = "CSS/Design";
+        $("#result").text(name + ", you are most compatible with: " + css);
+      }
+      if(result === c){
+        c = "C#/.NET";
+        $("#result").text(name + ", you are most compatible with: " + c);
+      }
+    } else { // all choices are selected equal amout of times, probably"
+        $("#result").text(name + ", you are compatible with any track");
+    }
   });
 });
-Courses
-Previous
-Next
-
-Search lessons
